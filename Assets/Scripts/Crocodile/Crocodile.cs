29d21d6975b8f887f2d1bfafbf1c23cf88e9fa3e
hegeteus <45102227+hegeteus@users.noreply.github.com>
2020-11-13 22:57:32 +0200

    Crocodile animations and movement

---
+++ Assets/Scripts/Crocodile/Crocodile.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocodile : MonoBehaviour
+{
+    // Public
+    public Animator animator;
+    public float movementSpeed;
+
+    // Method based
+    private bool walking;
+    private bool chomping;
+
+    // private
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponentInChildren<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Chomping = false;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Chomping = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Walking = false;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Walking = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MoveRight();
+    }
+
+    public bool Walking
+    {
+        get
+        {
+            return walking;
+        }
+        set
+        {
+            walking = value;
+            animator.SetBool("Walking", value);
+        }
+    }
+
+    public bool Chomping
+    {
+        get
+        {
+            return chomping;
+        }
+        set
+        {
+            chomping = value;
+            animator.SetBool("Chomping", value);
+        }
+    }
+
+    public void MoveRight()
+    {
+        rb.AddForce(transform.forward * movementSpeed * Time.deltaTime);
+        Walking = true;
+        Chomping = true;
+    }
+
+    public void StopMoving()
+    {
+        Chomping = false;
+        Walking = false;
+    }
+
+}
