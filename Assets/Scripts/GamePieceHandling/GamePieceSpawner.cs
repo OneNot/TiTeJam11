93ba0adf555bda76fce4e58d0fc319f660774679
Toni Hannula <www.1n07@gmail.com>
2020-11-14 01:37:53 +0200

    GamePieceSpawner 1.0

--- Assets/Scripts/GamePieceHandling/GamePieceSpawner.cs
+++ Assets/Scripts/GamePieceHandling/GamePieceSpawner.cs
@@ -8,18 +8,62 @@ public class GamePieceSpawner : MonoBehaviour
     [SerializeField]
     List<GameObject> GamePiecesToPool;
     List<GameObject> GamePiecePool;
+    public float DisableAtDistance = 500f;
+
+    private GamePiece latestPieceAdded = null;
 
     private void Awake() {
         Instance = this;
     }
 
     private void Start() {
+        GamePiecePool = new List<GameObject>();
         if(GamePiecesToPool != null)
         {
             for(int i = 0; i < GamePiecesToPool.Count; i++)
             {
-                //GamePiecePool.Add()
+                GameObject obj = Instantiate(GamePiecesToPool[i]);
+                obj.SetActive(false);
+                GamePiecePool.Add(obj);
             }
         }
+
+        SpawnRandomGamePiece();
+    }
+
+    private void Update() {
+        if(Input.GetButtonDown("Fire1"))
+            SpawnRandomGamePiece();
+    }
+
+    private void SpawnRandomGamePiece()
+    {
+        //get random object from pool
+        GameObject go = GamePiecePool[Random.Range(0, GamePiecePool.Count)];
+        
+        //if the object is already in use
+        if(go.activeSelf)
+        {
+            //duplicate a new one instead
+            go = Instantiate(go);
+            go.GetComponent<GamePiece>().duplicate = true;
+        }
+
+        go.SetActive(true);
+        
+        //if not the first piece
+        if(latestPieceAdded != null)
+        {
+            //position the new piece right after the previous one
+            go.transform.position = new Vector3(0f, 0f, latestPieceAdded.groundCollider.bounds.max.z + go.GetComponent<GamePiece>().groundCollider.transform.localScale.z/2);
+        }
+        else //first piece
+        {
+            //position the first piece
+            go.transform.position = Vector3.zero;
+        }
+
+        //update previous/latest piece
+        latestPieceAdded = go.GetComponent<GamePiece>();
     }
 }
