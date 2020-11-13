abc2423928a1abd0c5bc58b52167792182a682fd
hegeteus <45102227+hegeteus@users.noreply.github.com>
2020-11-13 23:47:06 +0200

    ground layer

---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponentInChildren<Rigidbody>();
+        if(rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CollisionActions()
+    {
+
+    }
+}
