47f50f74910ff2d38261faeb744c19be99a2e7bb
Toni Hannula <www.1n07@gmail.com>
2020-11-14 23:51:17 +0200

    Added PlayerState with HP handling

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public static PlayerState Instance;
+
+    public int MaxHP = 3;
+    private int currentHP;
+
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Update() {
+        if(currentHP <= 0)
+        {
+            print("Ya dead my guy");
+        }
+    }
+
+    public void ChangeHealth(int healthChange)
+    {
+        currentHP += healthChange;
+    }
+
+}
