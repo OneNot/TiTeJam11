27d6da7ea8d83eda091c9397522371f8cf499fff
hegeteus <45102227+hegeteus@users.noreply.github.com>
2020-11-14 05:08:33 +0200

    krokotiilin animaation parannuksia, 3d malleja, korjaus destroyable logiikkaan

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Destroyable : MonoBehaviour
 {
     private Rigidbody rb;
-    private float collisionTimeLimit = 0.2f;
+    private float collisionTimeLimit = 0.1f;
     private float collisionTimer;
+
+    [HideInInspector]
+    public bool crocodileTouched;
 
     private void Awake()
     {
@@ -31,7 +34,7 @@ public class Destroyable : MonoBehaviour
         rb.constraints = RigidbodyConstraints.None;
 
         if(_collision.gameObject.tag == "Crocodile" && addForce)
-        rb.AddRelativeForce(_collision.GetContact(0).point.normalized * 15, ForceMode.Impulse);
+        rb.AddRelativeForce(_collision.GetContact(0).point.normalized * 20f, ForceMode.Impulse);
     }
 
     public void InitialChecks() // Initial checks
@@ -52,8 +55,13 @@ public class Destroyable : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Crocodile" || collision.relativeVelocity.magnitude >= 6f)
+        if (collision.gameObject.tag == "Crocodile" || collision.relativeVelocity.magnitude >= 5f)
         {
+            if(collision.gameObject.tag == "Crocodile")
+            {
+                crocodileTouched = true;
+            }
+
             CollisionActions(collision);
         }
     }
@@ -61,10 +69,33 @@ public class Destroyable : MonoBehaviour
     //Failsafe
     private void OnCollisionStay(Collision collision)
     {
-        collisionTimer += Time.deltaTime;
-        if(collisionTimer > collisionTimeLimit)
+        // There is meticulous null checking here to ensure maximum foolproofness of using this script
+        bool thisCrocodileTouched = false;
+        bool otherCrocodileTouched = false;
+
+        Destroyable thisDestroyable = this;
+        Destroyable otherDestroyable = collision.gameObject.GetComponentInChildren<Destroyable>();
+
+        if (otherDestroyable == null)
+            otherDestroyable = collision.gameObject.GetComponentInParent<Destroyable>();
+
+        if (otherDestroyable != null)   // Once more to check if it is a lost cause, defaulting the boolean to false
         {
-            CollisionActions(collision, false);
+            otherCrocodileTouched = otherDestroyable.crocodileTouched;
+        }
+
+        if (thisDestroyable != null)
+        {
+            thisCrocodileTouched = thisDestroyable.crocodileTouched;
+        }
+
+        if (thisCrocodileTouched || otherCrocodileTouched)
+        {
+            collisionTimer += Time.deltaTime;
+            if (collisionTimer > collisionTimeLimit)
+            {
+                CollisionActions(collision, false);
+            }
         }
     }
 }
