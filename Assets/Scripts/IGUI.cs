e6a54ab9b3d32ad3f61d11a113f6b2dfa76bfe62
hegeteus <45102227+hegeteus@users.noreply.github.com>
2020-11-15 17:35:19 +0200

    score, pause

--- Assets/Scripts/IGUI.cs
+++ Assets/Scripts/IGUI.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IGUI : MonoBehaviour
 {
     public static IGUI Instance;
     public GameObject GameOver;
+    public GameObject PauseScreen;
+    public Text scoreText;
+    public Text scoreText2;
+
+    public float score = 0;
 
     private void Awake()
     {
@@ -22,7 +28,17 @@ public class IGUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+
+        if(!PauseScreen.activeSelf)
+        score += Time.deltaTime;
+
+        scoreText.text = "Score: " + Mathf.RoundToInt(score).ToString();
+        scoreText2.text = "Score: " + Mathf.RoundToInt(score).ToString();
+
     }
 
     public void ShowGameOverScreen()
@@ -30,6 +46,19 @@ public class IGUI : MonoBehaviour
         Time.timeScale = 0;
         GameOver.SetActive(true);
     }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        PauseScreen.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        PauseScreen.SetActive(false);
+    }
+
     public void Restart()
     {
         SceneManager.LoadScene(1);
